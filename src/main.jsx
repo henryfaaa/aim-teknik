d0493ca223894a1728a9efeb7316e7bff4fe91ea
Henry Fadhlu <henryfadhlu@gmail.com>
2026-04-20 08:12:52 +0700

    fix api proxy

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./router/AppRouter";
 import "./index.css";
+const API = import.meta.env.VITE_API_URL;
 
+const oldFetch = window.fetch;
+
+window.fetch = (url, options = {}) => {
+  if (typeof url === "string" && url.startsWith("/api")) {
+    url = API + url;
+  }
+  return oldFetch(url, options);
+};
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppRouter />
